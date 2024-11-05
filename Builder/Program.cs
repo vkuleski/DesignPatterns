89bf18aac91da8fed96a78fea5c5dffaccc71f04
Viktor Kuleski <vkuleski@outlook.com>
2024-11-05 21:59:51 +0100

    explain steps in comment

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -2,6 +2,12 @@
 
 using Builder;
 
+//1. Identify the distinct steps
+//2. Define each step as an interface
+//3. Add method (one or more) that move you to the next step
+//4. Have the last step build the underlying product
+//5. Implement the interfaces in the builder
+//6.Add an entry point in the builder, that returns the first step
 var pizza = Pizza.Builder.Start()
     .SetDough(dough=>dough
         .SetThickness(3)
